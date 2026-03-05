a6761b540dcab4971238db94e61416adafdc2c97
michael0x18 <michael0x18>
2026-03-05 14:37:52 -0600

    Toggle block

---
+++ src/lm_phy/toggle.sv
@@ -0,0 +1,16 @@
+`default_nettype none
+module toggle(
+	input wire clk,
+	input wire rst_n,
+	input wire pulse,
+	output reg data
+);
+	always @(posedge, pulse, negedge rst_n) begin
+		if(!rst_n) begin
+			data <= 1'b0;
+		end else begin
+			data <= ~data;
+		end
+	end
+endmodule
+`default_nettype wire
