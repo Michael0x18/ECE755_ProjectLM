d96d84d28a7d505d5d5fec967f2fb14eb6fc84ce
michael0x18 <michael0x18>
2026-03-05 09:36:24 -0600

    Instantiate pulse generators

--- src/lm_phy/lm_phy_rx.sv
+++ src/lm_phy/lm_phy_rx.sv
@@ -9,7 +9,14 @@ module lm_phy_rx(
 	input wire RX_ACK
 );
 
+wire[3:0] rx_pulse;
+wire rx_shift_nodelay;
+wire rx_shift;
 
+pulse_generator rxp(.rx(RX), .clk({4{clk}}), .rx_pulse(rx_pulse);
+
+assign rx_shift_nodelay = |rx_pulse;
+assign #25ns rx_shift = rx_shift_nodelay;
 
 endmodule
 `default_nettype wire
