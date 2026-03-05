6f2b97d3b90d50691d588c74b817b342487555da
michael0x18 <michael0x18>
2026-03-04 21:15:36 -0600

    Forgor lm_phy_rx

---
+++ src/lm_phy/lm_phy_rx.sv
@@ -0,0 +1,15 @@
+`default_nettype none
+module lm_phy_rx(
+	input wire clk,
+	input wire rst_n,
+	input wire [63:0] rx_out,
+	input wire rx_rdy,
+	output wire rx_vld,
+	output wire[3:0] RX,
+	input wire RX_ACK
+);
+
+
+
+endmodule
+`default_nettype wire
