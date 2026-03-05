44aaee44fe841ef99950cc8e6a5b98f04a35c4d4
michael0x18 <michael0x18>
2026-03-05 15:19:12 -0600

    Fix encoder

--- src/lm_phy/encoder4_2.sv
+++ src/lm_phy/encoder4_2.sv
@@ -1,16 +1,16 @@
 `default_nettype none
-module encoder 4_2(
+module encoder4_2(
 	input wire[3:0] in,
-	output wire[1:0] out
+	output reg[1:0] out
 );
 // Not glitchless - this is ok on this path
 always_comb begin
-	unique case (in) begin
+	unique case (in)
 		4'b0001: out = 2'h0;
 		4'b0010: out = 2'h1;
 		4'b0100: out = 2'h2;
 		4'b1000: out = 2'h3;
-	end
+	endcase
 end
 endmodule
 `default_nettype wire
