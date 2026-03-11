6ac7cd8fd466bb7a45b45f3f6782ad71d5f972a7
Michael Ferolito <michaelsunn101@gmail.com>
2026-03-10 19:57:39 -0500

    Forgot phy_top

--- src/lm_phy/lm_phy_top.sv
+++ src/lm_phy/lm_phy_top.sv
@@ -1,15 +1,15 @@
 `default_nettype none
-module lm_phy_top(
+module lm_phy_top #(parameter WIDTH = 64) (
 	input wire clk,
 	input wire rst_n_async,
 
 	// TX chip side interface
-	input wire[63:0] tx_in,
+	input wire[WIDTH-1:0] tx_in,
 	input wire tx_load,
 	output wire tx_done,
 
 	// RX chip side interface
-	output wire[63:0] rx_out,
+	output wire[WIDTH-1:0] rx_out,
 	output wire rx_vld,
 	input wire rx_rdy,
 
@@ -25,8 +25,8 @@ module lm_phy_top(
 	wire rst_n;
 	reset_sync u_rst_sync(.clk(clk), .rst_n_async(rst_n_async), .rst_n(rst_n));
 
-	lm_phy_tx u_lm_phy_tx(.clk(clk), .rst_n(rst_n), .TX(TX), .TX_ACK(TX_ACK), .tx_in(tx_in), .tx_load(tx_load), .tx_done(tx_done));
-	lm_phy_rx u_lm_phy_rx(.clk(clk), .rst_n(rst_n), .RX(RX), .RX_ACK(RX_ACK), .rx_out(rx_out), .rx_vld(rx_vld), .rx_rdy(rx_rdy));
+	lm_phy_tx #(WIDTH) u_lm_phy_tx(.clk(clk), .rst_n(rst_n), .TX(TX), .TX_ACK(TX_ACK), .tx_in(tx_in), .tx_load(tx_load), .tx_done(tx_done));
+	lm_phy_rx #(WIDTH) u_lm_phy_rx(.clk(clk), .rst_n(rst_n), .RX(RX), .RX_ACK(RX_ACK), .rx_out(rx_out), .rx_vld(rx_vld), .rx_rdy(rx_rdy));
 
 endmodule
 `default_nettype wire
