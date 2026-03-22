30ee93065686c292f71d0996ec8e3ed042ae0517
SUBRAMANYA VEDAANT NOOKALA <nookala2@wisc.edu>
2026-03-21 20:53:19 -0500

    made changes, rx_fsm needs to be tweaked to fix previous misunderstanding; combinational loop in lm_phy_rx needs to be found

--- src/lm_phy/lm_phy_rx.sv
+++ src/lm_phy/lm_phy_rx.sv
@@ -1,3 +1,5 @@
+//TODO: find combinational loop that causes this shit to crash!
+//i.e, jsut comment out lines until it stops lol
 `default_nettype none
 module lm_phy_rx #(
 	parameter WIDTH=64
@@ -7,15 +9,15 @@ module lm_phy_rx #(
 	output wire [WIDTH-1:0] rx_out,
 	input wire rx_rdy,
 	output wire rx_vld,
-	input wire[3:0] RX,
+	input wire  [3:0] RX,
 	output reg RX_ACK
 );
 
 wire[3:0] rx_pulse;
 wire rx_shift_nodelay;
-wire rx_shift;
+reg rx_shift;
 
-wire[3:0] _Q_n;
+wire[3:0] Q_n;
 wire[3:0] Q;
 wire[3:0] S;
 wire[3:0] R;
@@ -26,7 +28,7 @@ genvar i;
 generate
 	for(i = 0; i < 4; ++i) begin
 		pulse_generator rxp(.rx(RX[i]), .rx_pulse(rx_pulse[i]));
-		sr srl(.S(S[i]), .R(R[i]), .Q(Q[i]), .Q_n(_Q_n[i]));
+		sr srl(.S(S[i]), .R(R[i]), .Q(Q[i]), .Q_n(Q_n[i]));
 	end
 endgenerate
 
@@ -37,7 +39,9 @@ assign S[2] = R[0]|R[1]|R[3];
 assign S[3] = R[0]|R[1]|R[2];
 
 assign rx_shift_nodelay = |rx_pulse;
-assign #500ps rx_shift = rx_shift_nodelay;
+//assign  rx_shift = #500ps rx_shift_nodelay;
+always@(rx_shift_nodelay)
+  rx_shift<=#500ps rx_shift_nodelay;
 
 rx_fsm fsm(.clk(clk), .rst_n(rst_n), .rdy(rx_rdy), .vld(rx_vld), .rx_pulse(rx_shift), .ack_toggle(ack_toggle));
 
@@ -56,4 +60,5 @@ encoder4_2 enc(.in(Q), .out(rx_encoded));
 rx_shift_reg shift_reg(.shift_clk(rx_shift), .rst_n(rst_n), .shift_data(rx_encoded), .rx_out(rx_out));
 
 endmodule
+
 `default_nettype wire
--- src/lm_phy/rx_fsm.sv
+++ src/lm_phy/rx_fsm.sv
@@ -1,37 +1,72 @@
+//NOTE: upon reset (either via rst_n or clr_vld), vld is set to 0.  
+//after this, ack_toggle is ONLY sent via rx_pulse. 
+//clr_vld s asserted on a rdy_pulse from synchronous circuit
+//ignores rdy_pulse in that case. 
+//
+//NOTE: ready is a handshake signal paired with valid
+//- turn it on
+//- able to accept data
+//- ready is a **clear valid signal**
+//**RX will always come out of the reset condition BEFORE TX**
+//the initial ack pulse is wrong lol
+//
+//To indicate to the TX side that "hey, im listening", 
+//TODO: add parameterization to change bit width
 `default_nettype none
-module rx_fsm(
-    input wire clk,
-    input wire rst_n,
-    input wire rdy,
-    output reg vld,
-    input wire rx_pulse,
+module rx_fsm (
+    input  wire clk,
+    input  wire rst_n,
+    input  wire rdy,
+    output reg  vld,
+    input  wire rx_pulse,
     output wire ack_toggle
 );
 
-wire clr_vld;
-reg[4:0] counter;
-wire data_done;
-wire rdy_pulse;
-wire rdy_delay;
+  wire clr_vld;
+  reg [4:0] counter;
+  wire data_done;
+  wire rdy_pulse;
+  reg rdy_delay;
 
-always @(posedge rx_pulse, negedge clr_vld) begin
-    if(~rst_n) begin
-        counter <= 5'h10;
-        vld <= 1'b0;
+  //ERROR: when rdy_pulse is sent, causes block to evaluate. 
+  //rst_n is not true, hence causes it to decrement EVEN THOUGH 
+  //a signal has not been recieved. 
+  //solution: replace reset conditin with ~clr_vld?
+  always @(posedge rx_pulse, negedge clr_vld) begin
+    if (~clr_vld) begin
+      counter <= 5'h10;
+      vld <= 1'b0;
     end else begin
-        counter <= counter - 1;
-        vld <= (counter == 5'h1);
+      counter <= counter - 1;
+      vld <= (counter == 5'h1);
     end
-end
+  end
+
+  //maybe send a fixed-time pulse on the negative edge of rdy_pulse? 
+  //1) rdy is asserted
+  //2) generates a fixed-length pulse (rdy_pulse). 
+  //3) at the negedge of rdy_pulse, *then* send out the actual one?
+  reg rdy_pulse_delay;
+  always@(rdy_pulse)
+    rdy_pulse_delay<=#10ps rdy_pulse;
 
 
-// TODO replace this with standard cell components
-assign #100ps rdy_delay = rdy;
-assign rdy_pulse = (rdy & ~rdy_delay);
+  // TODO replace this with standard cell components
+  // TODO; replace this with a delay line
+  //assign #100ps rdy_delay = rdy;
+  always @(rdy, negedge rst_n) begin
+    if (~rst_n) rdy_delay <= 1'b0;
+    else rdy_delay <= #10ps rdy;
+  end
+  assign rdy_pulse = (rdy & ~rdy_delay);
+  //NOTE: causes an error if rx_pulse arrives too EARLY
+  //i.e, while rdy_pulse s still high, rx_pulse arrives, causing rdy_pulse to
+  //be one continuous pulse instead of 2; misses transmission, STALLS!
+  //assume that rx_pulse WILL arrive ONLY after rdy_pulse de-asserts? 
 
-assign ack_toggle = vld ? rdy_pulse : rx_pulse;
+  assign ack_toggle = (vld||counter==5'h10) ? rdy_pulse_delay : rx_pulse;
 
-assign clr_vld = rst_n & ~rdy_pulse;
+  assign clr_vld = rst_n & ~rdy_pulse;
 
 endmodule
 `default_nettype wire
