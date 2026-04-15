61e3c80a2160c90aca15b5fcb4074141fe4e0bf6
Josh <jmlaker@wisc.edu>
2026-04-14 23:23:02 -0500

    Made the testbenches account for stdcell

--- src/lm_phy/clock_gate_low.sv
+++ src/lm_phy/clock_gate_low.sv
@@ -4,21 +4,28 @@ module clock_gate_low(
     input wire en,
     output wire clk_gated
 );
-//reg q_L;
+
+`ifdef SIM
+
+reg q_L;
 // Infer latch
 // should use integrated clock gate from std cel library
 
-/*
+
 always_latch begin
     if(~clk) begin
         q_L = en;
     end
 end
-*/
 
-//assign clk_gated = clk & q_L;
+
+assign clk_gated = clk & q_L;
+
+`else
 
 sky130_fd_sc_hd__dlclkp_2 clk_gate(.GCLK(clk_gated),.GATE(en),.CLK(clk));
+
+`endif
 
 endmodule
 `default_nettype wire
