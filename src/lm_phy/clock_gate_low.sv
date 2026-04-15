ab3b9ead0e0b8fe2539f6ba4b91ea66f9003b9b3
SUBRAMANYA VEDAANT NOOKALA <nookala2@wisc.edu>
2026-04-14 20:53:10 -0500

    added clock gater cell

--- src/lm_phy/clock_gate_low.sv
+++ src/lm_phy/clock_gate_low.sv
@@ -4,16 +4,21 @@ module clock_gate_low(
     input wire en,
     output wire clk_gated
 );
-reg q_L;
+//reg q_L;
 // Infer latch
 // should use integrated clock gate from std cel library
+
+/*
 always_latch begin
     if(~clk) begin
         q_L = en;
     end
 end
+*/
 
-assign clk_gated = clk & q_L;
+//assign clk_gated = clk & q_L;
+
+sky130_fd_sc_hd__dlclkp_2 clk_gate(.GCLK(clk_gated),.GATE(en),.CLK(clk));
 
 endmodule
 `default_nettype wire
